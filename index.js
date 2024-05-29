9e462e3bb264b028693ea3a3a75469216a20cf95
SNEHAL RAHANGDALE <dushyant@Snehal.local>
2024-05-29 14:05:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// // Form validation script
+// document.getElementById('subscriptionForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     var emailField = document.getElementById('email');
+//     if (!emailField.checkValidity()) {
+//         emailField.classList.add('is-invalid');
+//     } else {
+//         emailField.classList.remove('is-invalid');
+//         emailField.value = '';
+//         alert('Subscription successful!');
+//     }
+// });
